f3145ece1fad962047515e345190824714353fa1
Tony Eneh <anthony.u.eneh@gmail.com>
2022-07-08 18:44:42 +0100

    Feat: banner component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Banner from '../components/Banner'
 import Header from '../components/Header'
 
 const Home: NextPage = () => {
@@ -11,6 +12,7 @@ const Home: NextPage = () => {
       </Head>
 
       <Header />
+      <Banner />
     </div>
   )
 }
